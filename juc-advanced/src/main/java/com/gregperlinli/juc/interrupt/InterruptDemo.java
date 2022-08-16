51b8e106a2462a56c002ca5da66759eb12685bd3
gregPerlinLi <lihaolin13@outlook.com>
2022-08-16 11:00:24 +0800

    feat():commit

--- juc-advanced/src/main/java/com/gregperlinli/juc/interrupt/InterruptDemo.java
+++ juc-advanced/src/main/java/com/gregperlinli/juc/interrupt/InterruptDemo.java
@@ -12,10 +12,30 @@ public class InterruptDemo {
     static AtomicBoolean atomicBoolean = new AtomicBoolean(false);
 
     public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            while ( true ) {
+                if ( Thread.currentThread().isInterrupted() ) {
+                    System.out.println("====> " + Thread.currentThread().getName() + "\t isInterrupted have modified to true, program stop");
+                    break;
+                }
+                System.out.println("====> Hello interrupt API...");
+            }
+        }, "t1");
+        t1.start();
+
+        // Pause the thread for a few seconds
+        try { TimeUnit.MILLISECONDS.sleep(200); } catch (InterruptedException e ) { e.printStackTrace(); }
+
+        new Thread(() -> {
+            t1.interrupt();
+        }, "t2").start();
+    }
+
+    private static void m2AtomicBoolean() {
         new Thread(() -> {
             while ( true ) {
                 if ( atomicBoolean.get() ) {
-                    System.out.println("====> " + Thread.currentThread().getName() + "\t isStop have modified to true, program stop");
+                    System.out.println("====> " + Thread.currentThread().getName() + "\t atomicBoolean have modified to true, program stop");
                     break;
                 }
                 System.out.println("====> Hello AtomicBoolean...");
