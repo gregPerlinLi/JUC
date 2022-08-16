5c41209e2f705d4c75caecda148efcdc1665468f
gregPerlinLi <lihaolin13@outlook.com>
2022-08-16 10:52:52 +0800

    feat():commit

--- juc-advanced/src/main/java/com/gregperlinli/juc/interrupt/InterruptDemo.java
+++ juc-advanced/src/main/java/com/gregperlinli/juc/interrupt/InterruptDemo.java
@@ -1,6 +1,7 @@
 package com.gregperlinli.juc.interrupt;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author gregPerlinLi
@@ -8,8 +9,28 @@ import java.util.concurrent.TimeUnit;
  */
 public class InterruptDemo {
     static volatile boolean isStop = false;
+    static AtomicBoolean atomicBoolean = new AtomicBoolean(false);
 
     public static void main(String[] args) {
+        new Thread(() -> {
+            while ( true ) {
+                if ( atomicBoolean.get() ) {
+                    System.out.println("====> " + Thread.currentThread().getName() + "\t isStop have modified to true, program stop");
+                    break;
+                }
+                System.out.println("====> Hello AtomicBoolean...");
+            }
+        }, "t1").start();
+
+        // Pause the thread for a few seconds
+        try { TimeUnit.MILLISECONDS.sleep(200); } catch (InterruptedException e ) { e.printStackTrace(); }
+
+        new Thread(() -> {
+            atomicBoolean.set(true);
+        }, "t2").start();
+    }
+
+    private static void m1Volatile() {
         new Thread(() -> {
             while ( true ) {
                 if ( isStop ) {
