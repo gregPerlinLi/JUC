273dc40724e66ac555c12e73f6f2cfa315a73f57
李浩霖 -gregPerlinLi- <lihaolin13@outlook.com>
2022-09-14 21:04:27 +0800

    feat():commit

---
+++ juc-advanced/src/main/java/com/gregperlinli/juc/base/TestDemo.java
@@ -0,0 +1,16 @@
+package com.gregperlinli.juc.base;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author gregPerlinLi
+ * @date 2022-09-14
+ */
+public class TestDemo {
+    volatile int age;
+
+    public static void main(String[] args) {
+
+    }
+}
