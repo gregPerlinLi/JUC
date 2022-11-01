e7a70167f81eed606d06d948c7b9cf5c05fcfa46
= <=>
2022-11-01 23:38:11 +0800

    feat():commit
    Signed-off-by: = <=>

---
+++ juc-advanced/src/main/java/com/gregperlinli/juc/volatiles/T1.java
@@ -0,0 +1,13 @@
+package com.gregperlinli.juc.volatiles;
+
+/**
+ * @author gregPerlinLi
+ * @date 2022-11-01
+ */
+public class T1 {
+    volatile boolean flag;
+
+    public static void main(String[] args) {
+        System.out.println("====> Hello T1 ...");
+    }
+}
