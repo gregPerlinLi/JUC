c5e2dc031f69619375fc900ac2bad7c5d3f68e0e
gregPerlinLi <lihaolin13@outlook.com>
2022-11-02 22:35:26 +0800

    feat():commit
    Signed-off-by: gregPerlinLi <lihaolin13@outlook.com>

--- juc-advanced/src/main/java/com/gregperlinli/juc/cas/AbaDemo.java
+++ juc-advanced/src/main/java/com/gregperlinli/juc/cas/AbaDemo.java
@@ -2,6 +2,7 @@ package com.gregperlinli.juc.cas;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicStampedReference;
 
 /**
  * @author gregPerlinLi
@@ -9,8 +10,10 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class AbaDemo {
     static AtomicInteger atomicInteger = new AtomicInteger(100);
+    static AtomicStampedReference<Integer> atomicStampedReference = new AtomicStampedReference<>(100, 1);
 
     public static void main(String[] args) {
+        // ABA happen
         new Thread(() -> {
             atomicInteger.compareAndSet(100, 101);
             // Pause the thread for a few milliseconds
@@ -22,5 +25,24 @@ public class AbaDemo {
             try { TimeUnit.MILLISECONDS.sleep(200); } catch (InterruptedException e ) { e.printStackTrace(); }
             System.out.println("====> " + atomicInteger.compareAndSet(100, 2022) + "\t ----> " + atomicInteger.get());
         }, "t2").start();
+        // ABA resolved
+        new Thread(() -> {
+            int stamp = atomicStampedReference.getStamp();
+            System.out.println("====> " + Thread.currentThread().getName() + "\t ----> Version1: " + stamp);
+            // Pause the thread for a few milliseconds
+            try { TimeUnit.MILLISECONDS.sleep(500); } catch ( InterruptedException e ) { e.printStackTrace(); }
+            atomicStampedReference.compareAndSet(100, 101, atomicStampedReference.getStamp(), atomicStampedReference.getStamp() + 1);
+            System.out.println("====> " + Thread.currentThread().getName() + "\t ----> Version2: " + atomicStampedReference.getStamp());
+            atomicStampedReference.compareAndSet(101, 100, atomicStampedReference.getStamp(), atomicStampedReference.getStamp() + 1);
+            System.out.println("====> " + Thread.currentThread().getName() + "\t ----> Version3: " + atomicStampedReference.getStamp());
+        }, "t3").start();
+        new Thread(() -> {
+            int stamp = atomicStampedReference.getStamp();
+            System.out.println("====> " + Thread.currentThread().getName() + "\t ----> Version1: " + stamp);
+            // Pause the thread for a few seconds
+            try { TimeUnit.SECONDS.sleep(1); } catch ( InterruptedException e ) { e.printStackTrace(); }
+            boolean b = atomicStampedReference.compareAndSet(100, 2022, stamp, stamp + 1);
+            System.out.println("====> " + b + "\t ----> " + atomicStampedReference.getReference() + "\t ----> Version3: " + atomicStampedReference.getStamp());
+        }, "t4").start();
     }
 }
