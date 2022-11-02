62dc8b7c780ff41555b9a48589501e9f16753d86
gregPerlinLi <lihaolin13@outlook.com>
2022-11-02 21:14:50 +0800

    feat():commit
    Signed-off-by: gregPerlinLi <lihaolin13@outlook.com>

---
+++ juc-advanced/src/main/java/com/gregperlinli/juc/atomic/AtomicReferenceDemo.java
@@ -0,0 +1,31 @@
+package com.gregperlinli.juc.atomic;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @author gregPerlinLi
+ * @date 2022-11-02
+ */
+public class AtomicReferenceDemo {
+    public static void main(String[] args) {
+        AtomicReference<User> userAtomicReference = new AtomicReference<>();
+        User z3 = new User("z3", 22);
+        User li4 = new User("li4", 28);
+        userAtomicReference.set(z3);
+        System.out.println("====> " + userAtomicReference.compareAndSet(z3, li4) + "\t Result: " + userAtomicReference.get().toString());
+        System.out.println("====> " + userAtomicReference.compareAndSet(z3, li4) + "\t Result: " + userAtomicReference.get().toString());
+    }
+}
+
+@Getter
+@ToString
+@AllArgsConstructor
+class User {
+    String username;
+    int age;
+}
