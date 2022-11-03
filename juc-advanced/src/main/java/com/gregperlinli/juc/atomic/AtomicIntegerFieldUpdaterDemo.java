42ec97dc6216b73f14700a31cb2e17681fad331c
gregPerlinLi <lihaolin13@outlook.com>
2022-11-03 19:58:36 +0800

    feat():commit
    Signed-off-by: gregPerlinLi <lihaolin13@outlook.com>

--- juc-advanced/src/main/java/com/gregperlinli/juc/atomic/AtomicIntegerFieldUpdaterDemo.java
+++ juc-advanced/src/main/java/com/gregperlinli/juc/atomic/AtomicIntegerFieldUpdaterDemo.java
@@ -1,6 +1,7 @@
 package com.gregperlinli.juc.atomic;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 
 /**
  * Operate some fields of non-threaded security objects in a thread-safe way.<br/>
@@ -19,7 +20,8 @@ public class AtomicIntegerFieldUpdaterDemo {
             new Thread(() -> {
                 try {
                     for (int j = 0; j < 1000; j++) {
-                        bankAccount.add();
+                        // bankAccount.add();
+                        bankAccount.transMoney(bankAccount);
                     }
                 } finally {
                     countDownLatch.countDown();
@@ -33,9 +35,24 @@ public class AtomicIntegerFieldUpdaterDemo {
 
 class BankAccount {
     String bankName = "CCB";
-    int money = 0;
+    /**
+     * The properties of the update object must be modified with {@code public volatile}.
+     */
+    public volatile int money = 0;
 
     public synchronized void add() {
         money++;
+    }
+
+    /**
+     * Because the attribute method type atom class of an object is an abstract class, you must use the static method newUpdater() to create an updater every time you use it, and you need to set the properties you want to update.
+     */
+    AtomicIntegerFieldUpdater<BankAccount> fieldUpdater = AtomicIntegerFieldUpdater.newUpdater(BankAccount.class, "money");
+
+    /**
+     * No synchronized is added to ensure high-performance atomic properties.
+     */
+    public void transMoney(BankAccount bankAccount) {
+        fieldUpdater.getAndIncrement(bankAccount);
     }
 }
