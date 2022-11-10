735bc2af2fdda1ec9aad6d990daf0d9c925c41a2
gregPerlinLi <lihaolin13@outlook.com>
2022-11-10 19:30:34 +0800

    feat():commit
    Signed-off-by: gregPerlinLi <lihaolin13@outlook.com>

--- juc-advanced/src/main/java/com/gregperlinli/juc/tl/ReferenceDemo.java
+++ juc-advanced/src/main/java/com/gregperlinli/juc/tl/ReferenceDemo.java
@@ -1,5 +1,6 @@
 package com.gregperlinli.juc.tl;
 
+import java.lang.ref.SoftReference;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -8,6 +9,23 @@ import java.util.concurrent.TimeUnit;
  */
 public class ReferenceDemo {
     public static void main(String[] args) {
+        SoftReference<MyObject> softReference = new SoftReference<>(new MyObject());
+        System.out.println("====> Soft reference: " + softReference.get());
+        System.gc();
+        // Pause the thread for a few seconds
+        try { TimeUnit.SECONDS.sleep(1); } catch ( InterruptedException e ) { e.printStackTrace(); }
+        System.out.println("====> gc after with enough memory: " + softReference.get());
+        try {
+            // Create a 20MB Object
+            byte[] bytes = new byte[20 * 1024 * 1024];
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("====> gc after with not enough memory: " + softReference.get());
+        }
+    }
+
+    private static void strongReference() {
         MyObject myObject = new MyObject();
         System.out.println("====> gc before: " + myObject);
         myObject = null;
