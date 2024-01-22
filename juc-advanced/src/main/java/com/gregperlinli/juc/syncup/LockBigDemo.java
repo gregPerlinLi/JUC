cd5f7169433046ead75656db5c2f0017c42f5338
gregPerlinLi <lihaolin13@outlook.com>
2024-01-22 14:18:28 +0800

    feat():Commit

--- juc-advanced/src/main/java/com/gregperlinli/juc/syncup/LockBigDemo.java
+++ juc-advanced/src/main/java/com/gregperlinli/juc/syncup/LockBigDemo.java
@@ -1,4 +1,35 @@
 package com.gregperlinli.juc.syncup;
 
+/**
+ * @author gregperlinli
+ * @date 2020/12/21 16:36
+ */
 public class LockBigDemo {
+    static Object objectLock = new Object();
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            // Before optimization
+            synchronized (objectLock) {
+                System.out.println("==> 111111");
+            }
+            synchronized (objectLock) {
+                System.out.println("==> 222222");
+            }
+            synchronized (objectLock) {
+                System.out.println("==> 333333");
+            }
+            synchronized (objectLock) {
+                System.out.println("==> 444444");
+            }
+
+            // Optimization by JIT
+            synchronized (objectLock) {
+                System.out.println("==> 111111");
+                System.out.println("==> 222222");
+                System.out.println("==> 333333");
+                System.out.println("==> 444444");
+            }
+        }, "t1").start();
+    }
 }
