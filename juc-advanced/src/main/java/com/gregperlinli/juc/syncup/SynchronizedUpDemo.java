8d2f219d3c44437d5522a80871b68621a6438fe7
gregPerlinLi <lihaolin13@outlook.com>
2024-01-22 13:12:41 +0800

    feat():Commit

--- juc-advanced/src/main/java/com/gregperlinli/juc/syncup/SynchronizedUpDemo.java
+++ juc-advanced/src/main/java/com/gregperlinli/juc/syncup/SynchronizedUpDemo.java
@@ -5,11 +5,36 @@ import org.openjdk.jol.info.ClassLayout;
 /**
  * @author gregperlinli
  * @date 2021/11/26 16:36
- * VM Options: -XX:+UseBiasedLocking -XX:BiasedLockingStartupDelay=0
+ * VM Options: -XX:-UseBiasedLocking
  */
 public class SynchronizedUpDemo {
+    private static Object objectLock = new Object();
     public static void main(String[] args) {
-        // Biased
+        new Thread(() -> {
+            synchronized (objectLock){
+                System.out.println(ClassLayout.parseInstance(objectLock).toPrintable());
+            }
+        }, "t1").start();
+
+        System.out.println("===================================================\n");
+
+        new Thread(() -> {
+            synchronized (objectLock){
+                System.out.println(ClassLayout.parseInstance(objectLock).toPrintable());
+            }
+        }, "t2").start();
+    }
+
+    private static void casLock() {
+        Object o = new Object();
+        new Thread(() -> {
+            synchronized (o) {
+                System.out.println(ClassLayout.parseInstance(o).toPrintable());
+            }
+        }, "t1").start();
+    }
+
+    private static void biasedLock() {
         Object o = new Object();
         System.out.println(ClassLayout.parseInstance(o).toPrintable());
 
